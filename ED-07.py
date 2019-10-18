915cefdff1a6cb5b86697117466cd27b7aed97cc
ProjetosIF37 <56701827+ProjetosIF37@users.noreply.github.com>
2019-10-17 23:21:18 -0300

    Add files via upload

---
+++ ED-07.py
@@ -0,0 +1,63 @@
+class No:
+    def __init__(self, dado = None, proximo = None):
+        self._dado = dado
+        self._proximo = proximo
+    def get_dado(self):
+        return self._dado
+    def set_dado(self, novoDado):
+        self._dado = novoDado
+    def get_proximo(self):
+        return self._proximo
+    def set_proximo(self, outro):
+        self._proximo = outro
+    def size(self):
+        return len(self._dado)
+    def _str_(self):
+        return "{}".format(self._dado)
+
+class Pilha:
+    def __init__(self, head = None):
+        self._head = head
+    def remove(self):
+        self.head = self._head.get_proximo()
+    def add(self, no):
+        no.set_proximo(self._head)
+        self._head = no
+
+class Fila:
+    def __init__(self, head = No()):
+        self._head = head
+        self._size = 0 
+    def remove(self):
+        self._head = self._head.get_proximo()
+        self._size -= 1
+    def add(self, no):
+        p = self._head
+        while(p.get_proximo() != None):
+            p = p.get_proximo()
+        p.set_proximo(no)
+        self._size += 1
+    def size(self):
+        return self._size
+    def insert(self, index,elem):
+            if index == 0 :
+                no = No(elem)
+                no.set_proximo(self._head)
+                self._head = no
+    def Mostrar(self, index):
+        p =self._head
+        for i in range (index):
+            if p.get_proximo() != None:
+                p.get_proximo
+            return print(p)
+                
+
+fil = Fila()
+a = No(1)
+b = No(2)
+fil.add(a)
+fil.add(b)
+print(fil.insert(2,3))
+print(fil.size())
+fil.Mostrar(0)
+
