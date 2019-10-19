a78f33b755b0efa9f27c23b0cb4a5ecfedb828c6
ProjetosIF37 <56701827+ProjetosIF37@users.noreply.github.com>
2019-10-18 23:04:23 -0300

    Add files via upload

--- Lista.py
+++ Lista.py
@@ -11,7 +11,9 @@ class Lista:
                 p = p.get_proximo()
         return print(p)
     def isEmpty(self): #Vazia
-        return self._head == None
+        if self._head == None:
+            return True
+        else: return False
     def size(self): #Tamanho
         p = self._head
         count = 0
@@ -19,34 +21,59 @@ class Lista:
             count = count + 1
             p = p.get_proximo()
         return count
-    def insert(self, index, elem):
+    def insert(self, index, elem): #Inserir
             if index == 0:
                 no = No(elem)
                 no.set_proximo(self._head)
                 self._head = no
-            if index == self.lenght() - 1:
+            if index > self.lenght():
+                print ( ' indice invalido ')
+            else:
                 no = No(elem)
-                p = self._head
-                while(p.get_proximo() != None):
-                    p = p.get_proximo()
-                p.set_proximo(no)
+                q = self._head
+                for i in range(index -1):
+                    if q.get_proximo() != None:
+                        q = q.get_proximo()
+                no.set_proximo(q.get_proximo())
+                q.set_proximo(no)
+    def remove(self,index): #Remover um item no indice
+        if index == 0:
+            self._head = self._head.get_proximo()
+        if index > self.lenght():
+            print ( ' indice invalido ')
+        else:
+            p = self._head
+            q = self._head.get_proximo()
+            for i in range(index - 1):
+                q = q.get_proximo()
+                p = p.get_proximo()
+            p.set_proximo(q.get_proximo())
+            q.set_proximo(p)
+
     def lenght(self):
-        start = self._head
-        size = 0
-        while (start.get_proximo() != None):
-            size += 1
-            start = start.get_proximo()
-        return size
+        p = self._head
+        count = 0
+        while (p!= None):
+            count += 1
+            p = p.get_proximo()
+        return count
     def printar_all(self):
         p = self._head
-        while(p !=None):
+        while(p != None):
             print(p)
             p = p.get_proximo()
+    def show(self, index):
+        p = self._head
+        for i in range(index):
+            p = p.get_proximo()
+        return p
 
-p = No(2)
-lista = Lista(p)
-lista.insert(No(5), 0)
-lista.insert(No(5), 0)
-lista.insert(No(5), 0)
+
+lista = Lista()
+lista.insert(0, No(0))
+lista.insert(1, No(1))
+lista.insert(4, No(2))
+lista.remove(5)
+lista.remove(1)
 lista.printar_all()
-print(lista.lenght())
+print(f'tamanho: {lista.lenght()}')
