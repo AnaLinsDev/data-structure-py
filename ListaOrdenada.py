dcdc3a838fead60a481e560465a8d17f2a56444f
8b1tz <53948477+8b1tz@users.noreply.github.com>
2019-10-25 00:27:19 -0300

    Update ListaOrdenada.py

--- ListaOrdenada.py
+++ ListaOrdenada.py
@@ -50,19 +50,22 @@ class Lista:
             q.set_proximo(p)
 
     def ordenar(self): #Ordena de forma crescente!
-        ordem = False
-        p = self._head
-        q = p.get_proximo()
-        while not ordem:
-            ordem = True
-            while q.get_proximo() != None: #Se o get proximo for None O whuile de cima será True.
-                if p.get_dado().get_ano() > q.get_dado().get_ano():
-                    ordem = False #Isso vai fazer que volte para o outro while quando acabar esse
-                    aux = p.get_dado()
-                    p.set_dado(q.get_dado())
-                    q.set_dado(aux)
-                p = q
-                q = q.get_proximo()
+        if self._head == None:
+            print("Não dá para Ordenar!"
+        else:
+            ordem = False
+            p = self._head
+            q = p.get_proximo()
+            while not ordem:
+                ordem = True
+                while q.get_proximo() != None: #Se o get proximo for None O whuile de cima será True.
+                    if p.get_dado().get_ano() > q.get_dado().get_ano():
+                        ordem = False #Isso vai fazer que volte para o outro while quando acabar esse
+                        aux = p.get_dado()
+                        p.set_dado(q.get_dado())
+                        q.set_dado(aux)
+                    p = q
+                    q = q.get_proximo()
     def length(self): #Ver o tamanho
         p =self._head
         count = 0
